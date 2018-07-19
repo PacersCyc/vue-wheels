776e1a4e2d630cbba6692243f80d2a68db3ab1a2
PacersCyc <452720855@qq.com>
2018-07-19 23:54:44 +0800

    将button改为单文件组件，并用parcel构建

---
+++ src/button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="c-button">按钮</button>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped lang="scss">
+  .c-button{
+    font-size: var(--font-size);
+    height:var(--button-height);
+    padding:0 1em;
+    border-radius: var(--border-radius);
+    border:1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover{
+      border-color:var(--border-color-hover);
+    }
+    &:active{
+      background: var(--button-active-bg);
+    }
+    &:focus{
+      outline: none;
+    }
+  }
+
+</style>
